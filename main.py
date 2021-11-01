007aed82426910ce317357f8b89c9d0a7755b6f1
Eko SW <swdev.bali@gmail.com>
2021-11-01 16:20:40 +0700

    initial code

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This project will get the latest information about earthquake from bmkg.go.id
+"""
+author = 'Eko SW'
+date = '1 November 2022'
+url = 'https://www.bmkg.go.id/'
+
+import requests
+print('Hello World!')
