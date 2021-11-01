b29e46bab7768334af72704f1d2d63842e2279e7
Eko SW <swdev.bali@gmail.com>
2021-11-01 16:48:23 +0700

    final

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 """
 This project will get the latest information about earthquake from bmkg.go.id
 """
+import requests
 from bs4 import BeautifulSoup
 
 author = 'Eko SW'
 date = '1 November 2022'
 url = 'https://www.bmkg.go.id/'
 
-import requests
 result = requests.get(url)
 result = BeautifulSoup(result.text, 'html.parser')
-print(result.title.string)
+divTag = result.find("span", {"class": "waktu"})
+latest_earthquake = divTag.string
+print(latest_earthquake)
