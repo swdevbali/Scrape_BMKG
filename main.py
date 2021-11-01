4f4ce033d4aa1b5ec3f7f2c6450688ff452813d2
Eko SW <swdev.bali@gmail.com>
2021-11-01 16:33:58 +0700

    scrape

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ date = '1 November 2022'
 url = 'https://www.bmkg.go.id/'
 
 import requests
-print('Hello World!')
+result = requests.get(url)
+print(result.status_code)
+print(result.text)
+
+result = BeautifulSoup`
