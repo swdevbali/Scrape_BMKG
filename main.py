fa70e22e4e8c53ec56ae070168595e0e601c9a4a
Eko SW <swdev.bali@gmail.com>
2021-11-01 16:40:52 +0700

    scrape title

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 """
 This project will get the latest information about earthquake from bmkg.go.id
 """
+from bs4 import BeautifulSoup
+
 author = 'Eko SW'
 date = '1 November 2022'
 url = 'https://www.bmkg.go.id/'
 
 import requests
 result = requests.get(url)
-print(result.status_code)
-print(result.text)
-
-result = BeautifulSoup`
+result = BeautifulSoup(result.text, 'html.parser')
+print(result.title.string)
